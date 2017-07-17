acb9536f1ec1bae5f302d629aced901e7db03677
Julia Kreger <juliaashleykreger@gmail.com>
2017-07-17 18:48:57 +0000

    Change theme to openstackdocs
    Change-Id: Iff5f94fb956b30feda1879d987c0cb4139a1ea9a

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'openstackdocstheme'
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
@@ -55,7 +55,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 # html_theme_path = ["."]
-# html_theme = '_theme'
+html_theme = 'openstackdocs'
 # html_static_path = ['static']
 
 # Output file base name for HTML help builder.
