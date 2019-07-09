f9a62fa6068e4d72adf2040f74c5cd3eb9c60001
Ilya Etingof <etingof@gmail.com>
2019-07-09 17:17:41 +0200

    Low case `ParamTypes` in received `MessageRegistry`
    It turns out, that some implementations encountered in the wild waters
    use different cases for (at least) `ParamTypes` enumerations in
    `MessageRegistry` resource. In the schema [1] these enumerations
    are all in low case.
    
    To make sushy operational, this patch just ignores the case of
    these enumerations. It's still an open question as to how strict the
    case requirement is with Redfish (/cc Davy Jones).
    
    1. https://redfish.dmtf.org/schemas/v1/MessageRegistry.v1_3_0.json
    
    Change-Id: I107b689b8f4e27dd7331473124f7108d151cc55b

--- sushy/resources/registry/message_registry.py
+++ sushy/resources/registry/message_registry.py
@@ -37,7 +37,7 @@ class MessageDictionaryField(base.DictionaryField):
 
     param_types = base.Field('ParamTypes',
                              adapter=lambda x:
-                                 [res_maps.PARAMTYPE_VALUE_MAP[v]
+                                 [res_maps.PARAMTYPE_VALUE_MAP[v.lower()]
                                   for v in x])
     """Mapped MessageArg types, in order, for the message"""
 
